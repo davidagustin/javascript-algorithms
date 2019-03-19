9f3572869a4cc7316ee702836e489eb3147ead1a
David Agustin <davidsyagustin@gmail.com>
2019-03-18 18:10:20 -0700

    Make Comments for combineWithoutRepetions

--- src/algorithms/sets/combinations/combineWithoutRepetitions.js
+++ src/algorithms/sets/combinations/combineWithoutRepetitions.js
@@ -4,9 +4,11 @@
  * @return {*[]}
  */
 export default function combineWithoutRepetitions(comboOptions, comboLength) {
+  console.log('In this frame combineWithRepetitions are comboOptions:', comboOptions, ' comboLength:', comboLength)
   // If the length of the combination is 1 then each element of the original array
   // is a combination itself.
   if (comboLength === 1) {
+    console.log('comboOptions.map(comboOption => [comboOption]) is ', comboOptions.map(comboOption => [comboOption]));
     return comboOptions.map(comboOption => [comboOption]);
   }
 
@@ -16,17 +18,24 @@ export default function combineWithoutRepetitions(comboOptions, comboLength) {
   // Extract characters one by one and concatenate them to combinations of smaller lengths.
   // We need to extract them because we don't want to have repetitions after concatenation.
   comboOptions.forEach((currentOption, optionIndex) => {
+    console.log('forEach of comboOptions with currentOption as ', currentOption, ' and optionIndex as ', optionIndex)
     // Generate combinations of smaller size.
     const smallerCombos = combineWithoutRepetitions(
       comboOptions.slice(optionIndex + 1),
       comboLength - 1,
     );
 
+    console.log('moved back a frame and going into concatenation with smallerCombos as:', smallerCombos);
+
     // Concatenate currentOption with all combinations of smaller size.
     smallerCombos.forEach((smallerCombo) => {
+      console.log('with currentOption ', currentOption, ' and smallerCombo:', smallerCombo) ;
       combos.push([currentOption].concat(smallerCombo));
+      console.log('combos.push([currentOption].concat(smallerCombo)) is', combos);
     });
   });
 
   return combos;
 }
+
+console.log(combineWithoutRepetitions(['rock', 'paper', 'scissors'], 2));
